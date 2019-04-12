581c6a5f304e4f3b57be81b62f5f77a42f79d71d
Vasili <anoshin45@gmail.com>
2019-04-12 10:15:11 -0700

    add newComponents

---
+++ mobile-flashcards/components/AddDeck.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
+
+class AddDecks extends Component {
+    render() {
+    console.log('inside AddDecks')
+    return (
+      <View>
+        <Text>This is Add Deck !!!!</Text>
+      </View>
+    );
+  }
+}
+
+export default AddDecks
---
+++ mobile-flashcards/components/Decks.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
+
+class Decks extends Component {
+    render() {
+    console.log('inside Decks')
+    return (
+      <View>
+        <Text>This is Decks !!!!</Text>
+      </View>
+    );
+  }
+}
+
+export default Decks
