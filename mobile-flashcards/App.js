d258ea8139ae4aa2aff498cf8e0c41ae4c2c0a3c
Vasili <anoshin45@gmail.com>
2019-04-12 10:14:54 -0700

    add Naviagtion

--- mobile-flashcards/App.js
+++ mobile-flashcards/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , StatusBar} from 'react-native';
+import TabNav from './components/TabNav';
+import { purple, white } from './utils/colors'
+import { Constants } from 'expo'
+import { createStackNavigator,createAppContainer  } from 'react-navigation'
+
+// const MainNavigator = createAppContainer(createStackNavigator({
+//     home: {
+//       screen: TabNav,
+//       navigationOptions: {
+//         header: null,
+//       },
+//     },
+//   }));
+
+function ManualStatusBar ({backgroundColor, ...props}) {
+    return (
+      <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+        <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+      </View>
+    )
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flex: 1}}>
+       <ManualStatusBar backgroundColor={purple} barStyle="light-content" />
+        {/* <MainNavigator /> */}
+        <TabNav />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile-flashcards/components/TabNav.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { createBottomTabNavigator, createAppContainer, createMaterialTopTabNavigator } from 'react-navigation';
+import { Platform } from 'react-native';
+import { purple, white } from '../utils/colors';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import Decks from './Decks';
+import AddDeck from './AddDeck';
+
+console.log('before route')
+const router = {
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) =>
+        Platform.OS === 'android' && <Ionicons name="ios-bookmarks" size={30} color={tintColor} />,
+    },
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({ tintColor }) =>
+        Platform.OS === 'android' && <FontAwesome name="plus-square" size={30} color={tintColor} />,
+    },
+  }
+};
+
+const navigationOptions = {
+  tabBarOptions: {
+    showIcon: true,
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      padding: 10,
+      height: Platform.OS === 'ios' ? 60 : 'auto',
+      fontSize: 18,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    },
+  },
+};
+
+console.log('after route')
+console.log(router)
+
+const TabNav =
+  Platform.OS === 'ios'
+    ? createBottomTabNavigator(router, navigationOptions)
+    : createMaterialTopTabNavigator(router, navigationOptions);
+ 
+ console.log(TabNav)
+ console.log('After TabNav')
+
+export default createAppContainer(TabNav);
